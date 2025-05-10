c38ff96d4c2f734aae42ba5afab15c6ba54f2256
Alexei Matveev <alexei.matveev@gmail.com>
2025-05-10 15:29:00 +0200

    AM: update deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [cheshire "5.11.0"]
-                 [clj-http "3.12.3"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [cheshire "6.0.0"]
+                 [clj-http "3.13.0"]]
   :main genko.core
   :repl-options {:init-ns genko.core})
