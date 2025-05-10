6437f15c6bb2f829be2d8ae032b200bdf9e46ce7
Alexei Matveev <alexei.matveev@gmail.com>
2025-05-10 14:20:48 +0200

    AM: config with url & key

--- src/genko/core.clj
+++ src/genko/core.clj
@@ -5,10 +5,10 @@
 
 (defn chat-completion
   "Example usage of OpenAI Chat Completion API."
-  [prompt]
+  [config prompt]
   (let [model "gpt-4o"
-        api-key (System/getenv "OPENAI_API_KEY")
-        base-url (System/getenv "OPENAI_API_BASE_URL")
+        api-key (:api-key config)
+        base-url (:base-url config)
         url (str base-url "/chat/completions")
         headers {"Authorization" (str "Bearer " api-key)
                  "Content-Type" "application/json"}
@@ -23,9 +23,11 @@
 (defn -main
   "Main entry point: reads a prompt from stdin, calls chat-completion, prints result."
   [& _]
-  (print "Enter your prompt: ")
-  (flush)
-  (let [prompt (read-line)
-        response (chat-completion prompt)]
-    (println "OpenAI response:\n" response)))
+  (let [config {:api-key (System/getenv "OPENAI_API_KEY")
+                :base-url (System/getenv "OPENAI_API_BASE_URL")}]
+    (print "Enter your prompt: ")
+    (flush)
+    (let [prompt (read-line)
+          response (chat-completion config prompt)]
+      (println "OpenAI response:\n" response))))
 
