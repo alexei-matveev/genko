6634bd6db555e02251c936f5b1edddd6f61b4578
Alexei Matveev <alexei.matveev@gmail.com>
2025-05-19 22:37:03 +0200

    AM: pltzhalter

---
+++ src/genko/server.clj
@@ -0,0 +1,9 @@
+;;
+;; Implements basic /chat/completions and /models endpoints of the
+;; OpenAI server protocoll. Sometime.
+;;
+(ns genko.server
+  (:require
+   [cheshire.core :as json]))
+
+;; TBD
