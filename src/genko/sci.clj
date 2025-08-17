b9f3ed0ef739d7c96064b7dc087aeba618d13d35
Alexei Matveev <alexei.matveev@gmail.com>
2025-08-17 17:18:28 +0200

    AM: it is quite a lot

--- src/genko/sci.clj
+++ src/genko/sci.clj
@@ -2,18 +2,27 @@
   (:require
    [sci.core :as sci]))
 
-;; (genko.sci/eval-string "(all-ns)") =>
-;; (#object[sci.lang.Namespace ... "user"]
-;;  #object[sci.lang.Namespace ... "clojure.core"]
-;;  #object[sci.lang.Namespace ... "clojure.set"]
-;;  #object[sci.lang.Namespace ... "Math"]
-;;  #object[sci.lang.Namespace ... "clojure.edn"]
-;;  #object[sci.lang.Namespace ... "clojure.repl"]
-;;  #object[sci.lang.Namespace ... "clojure.string"]
-;;  #object[sci.lang.Namespace ... "clojure.walk"]
-;;  #object[sci.lang.Namespace ... "clojure.template"])
 (defn eval-string [clojure-code]
   (sci/eval-string clojure-code
                    {:namespaces {'Math {'sin Math/sin
                                         'cos Math/cos
                                         'pow Math/pow}}}))
+
+;; To get an idea about "attack surface" without explicit allow-list:
+;;
+;; (eval-string "(all-ns)") =>
+;; (#object[sci.lang.Namespace ... "user"]
+;;  #object[sci.lang.Namespace ... "clojure.core"]
+;;  #object[sci.lang.Namespace ... "clojure.set"]
+;;  #object[sci.lang.Namespace ... "Math"]               <- We created that!
+;;  #object[sci.lang.Namespace ... "clojure.edn"]
+;;  #object[sci.lang.Namespace ... "clojure.repl"]
+;;  #object[sci.lang.Namespace ... "clojure.string"]
+;;  #object[sci.lang.Namespace ... "clojure.walk"]
+;;  #object[sci.lang.Namespace ... "clojure.template"])
+(comment
+  (eval-string "(count (all-ns))") => 9
+  (count (eval-string "(clojure.repl/dir-fn (find-ns 'user))")) => 0
+  (count (eval-string "(clojure.repl/dir-fn (find-ns 'Math))")) => 3
+  (count (eval-string "(clojure.repl/dir-fn (find-ns 'clojure.core))")) => 563
+  (count (eval-string "(clojure.repl/dir-fn (find-ns 'clojure.repl))")) => 10)
