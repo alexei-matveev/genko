d218da3475080f00f86ae290746440cde91a19fd
Alexei Matveev <alexei.matveev@gmail.com>
2025-08-17 16:38:42 +0200

    AM: more than one ns ist available in sci

--- src/genko/sci.clj
+++ src/genko/sci.clj
@@ -2,6 +2,16 @@
   (:require
    [sci.core :as sci]))
 
+;; (genko.sci/eval-string "(all-ns)") =>
+;; (#object[sci.lang.Namespace ... "user"]
+;;  #object[sci.lang.Namespace ... "clojure.core"]
+;;  #object[sci.lang.Namespace ... "clojure.set"]
+;;  #object[sci.lang.Namespace ... "Math"]
+;;  #object[sci.lang.Namespace ... "clojure.edn"]
+;;  #object[sci.lang.Namespace ... "clojure.repl"]
+;;  #object[sci.lang.Namespace ... "clojure.string"]
+;;  #object[sci.lang.Namespace ... "clojure.walk"]
+;;  #object[sci.lang.Namespace ... "clojure.template"])
 (defn eval-string [clojure-code]
   (sci/eval-string clojure-code
                    {:namespaces {'Math {'sin Math/sin
