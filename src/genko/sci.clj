a3a0386eb1aeef6e4a74ca2579be5980127524bc
Alexei Matveev <alexei.matveev@gmail.com>
2025-08-18 13:35:32 +0200

    AM: doall does the trick

--- src/genko/sci.clj
+++ src/genko/sci.clj
@@ -1,6 +1,9 @@
 (ns genko.sci
+  "So far a very slim interface to sandboxed Babashka Small Clojure
+  Interpreter (SCI)"
   (:require
    [sci.core :as sci]))
+
 
 (defn eval-string [clojure-code]
   (sci/eval-string clojure-code
@@ -8,35 +11,60 @@
                                         'cos Math/cos
                                         'pow Math/pow}}}))
 
-;; For internal use:
+;; For debugging:
 (defn- eval-sexp [sexp]
   (eval-string (str sexp)))
 
-;; To get an idea about "attack surface" without explicit allow-list:
+;; To get an idea about "attack surface" without explicit
+;; allow-list. Much the staff here is to decipher the *ctx* issue with
+;; lazy seqs.
 (comment
+  ;; NOTE: Closures  such as (fn []  (find-ns 'user)) or calls  to the
+  ;; likes of  `find-ns` inside of a  for-loop (lazy seq) are  no more
+  ;; closures over "SCI context" sci.ctx-store/*ctx*. I dont quite get
+  ;; it, but see the issue & changelog in the upstream project [1, 2].
+  ;;
+  ;; [1] https://github.com/babashka/sci/issues/989
+  ;; [2] https://github.com/babashka/sci/blob/master/CHANGELOG.md#01046-2025-06-18
+
+  ;; (def _ctx (sci/init {}))
+  ;; (keys _ctx) =>
+  ;; (:bindings :env :features :readers :reload-all :check-permissions
+  ;;  :allow :deny :reify-fn :proxy-fn :main-thread-id)
+  ;; This calls `alter-var-root` for dynamic var `*ctx*`:
+  ;; (sci.ctx-store/reset-ctx! _ctx)
+  ;; (sci.ctx-store/reset-ctx! nil)
+
   (eval-sexp '(count (all-ns))) => 9
-  (eval-sexp '(map str (all-ns)))
+  (eval-sexp '(doall (map str (all-ns)))) ; does not fail without `doall`
   =>
   ("user" "clojure.core" "clojure.set"
    "Math"                               ; <- we created that!
    "clojure.edn" "clojure.repl" "clojure.string" "clojure.walk" "clojure.template")
 
-  (eval-sexp '(let [ns 'user] (count (clojure.repl/dir-fn (find-ns ns))))) => 0
-  (eval-sexp '(count (clojure.repl/dir-fn (find-ns 'Math)))) => 3
-  (eval-sexp '(count (clojure.repl/dir-fn (find-ns 'clojure.core)))) => 563
-  (eval-sexp '(count (clojure.repl/dir-fn (find-ns 'clojure.repl)))) => 10
-
-  ;; FIXME: somethig is wrong with (find-ns) inside of a lazy for-loop
-  ;; or seq instead of a (strict?) let-form! See the issue in the
-  ;; upstream project [1].
+  ;; Number of bindings pro namespace. NOTE: `doall` *inside* SCI is
+  ;; important for lazy sequence such as the `for`-expression here!
+  ;; once you leave the dynamic context of the SCI you cannot count on
+  ;; daynamic `*ctx*` having a correct value!
   ;;
-  ;; [1] https://github.com/babashka/sci/issues/989
-  (eval-sexp '(for [ns ['user]] (find-ns ns))) ; => "No context found in: sci.ctx-store/*ctx*"
-  (eval-sexp '(map find-ns ['user]))           ; => Same!
-  (map find-ns ['user])                        ; => (#namespace[user])
-  (eval-sexp '(mapv find-ns ['user])) ; => [#object[sci.lang.Namespace 0x54f40d66 "user"]]
+  ;;     (eval-sexp '(for [ns ['user]] (find-ns ns)))   ; WRONG!
+  ;;     => Exception with "No context found in: sci.ctx-store/*ctx* ..."
+  (eval-sexp
+   '(doall
+     (for [ns (all-ns)]
+       [(str ns) (count (clojure.repl/dir-fn ns))])))
+  =>
+  (["user" 0]
+   ["clojure.core" 563]
+   ["clojure.set" 12]
+   ["Math" 3]                           ; 0 <> 3 without `doall`
+   ["clojure.edn" 2]
+   ["clojure.repl" 10]
+   ["clojure.string" 21]
+   ["clojure.walk" 10]
+   ["clojure.template" 2])
 
-  ;; Even without `find-ns` we still do need strict `mapv` here:
+  ;; Alternatively use `mapv` instead of lazy seq:
   (eval-sexp
    '(let [ns-objects (all-ns)
           dirs (mapv clojure.repl/dir-fn ns-objects)]
